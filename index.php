e1970ab9ac3ca208c4e314e46376b34512859c7e
cerealcrab <torshammer@gmx.de>
2016-01-13 12:02:26 +0100

    github datei local commit

--- index.php
+++ index.php
@@ -1,4 +1,11 @@
 <?php
-echo "Hello";
-echo "Second Hello";
+
+$view = isset($_GET['view']) ? $_GET['view'] : 'anzeige';
+$id = isset($_REQUEST['id']) ? $_REQUEST['id'] : '0';
+// action überprüfen ob id 0 ist
+if ($id < 1) {
+    $view = 'fehler';
+}
+// kurzform vom switch case
+include './view/' . $view . '.php';
 ?>
---
+++ view/fehler.php
@@ -0,0 +1,7 @@
+<?php
+include 'view/header.php';
+?>
+<input type="button" value="OK" formaction="index.php"/>
+<?php
+include 'view/footer.php';
+?>
